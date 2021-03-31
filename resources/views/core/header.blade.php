ab3d77a971e0c893b1b6fa45310ad68172a592b7
dvhungg123@gmail.com <dvhungg123@gmail.com>
2021-03-31 19:58:23 +0700

    bo fullscreen

--- resources/views/core/header.blade.php
+++ resources/views/core/header.blade.php
@@ -5,15 +5,10 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href={{route('home')}} class="nav-link">{{__('Trang chủ')}}</a>
+            <a href={{ route('home') }} class="nav-link">{{ __('Trang chủ') }}</a>
         </li>
     </ul>
     <ul class="navbar-nav ml-auto">
-        <li>
-        <a class="nav-link" data-widget="fullscreen" href="#"  role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-    </li>
     </ul>
 
 
@@ -23,48 +18,47 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href={{route('home')}} class="brand-link" >
-        <img src="{{asset('img/logo.jpeg')}}" alt="STi Logo"
-             class="brand-image img-circle elevation-3"
-             style="opacity: .8">
+    <a href={{ route('home') }} class="brand-link">
+        <img src="{{ asset('img/logo.jpeg') }}" alt="STi Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
         <span class="brand-text font-weight-light" style="padding-left: 90px"></span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
-            <div class="image">
-                <a href=#  class="btn btn-secondary dropdown-toggle" role="button" id="dropdownMenuLink"
-                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <div class="image">
+            <a href=# class="btn btn-secondary dropdown-toggle" role="button" id="dropdownMenuLink"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
-                    <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2"
-                         alt="User Image">
-                </a>
-                <div class="dropdown-menu" style="position: fixed" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item" href="{{route('profile.index')}}" style="color: black">Hồ sơ</a>
-                    <a class="dropdown-item" href="{{route('logOut')}}" style="color: black">Đăng xuất</a>
-                </div>
+                <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+            </a>
+            <div class="dropdown-menu" style="position: fixed" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="{{ route('profile.index') }}" style="color: black">Hồ sơ</a>
+                <a class="dropdown-item" href="{{ route('logOut') }}" style="color: black">Đăng xuất</a>
             </div>
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="info">
-                    <a class="d-block">Xin chào: {{\Illuminate\Support\Facades\Auth::user()->name}}</a>
-                </div>
-            </div>  
+        </div>
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="info">
+                <a class="d-block">Xin chào: {{ \Illuminate\Support\Facades\Auth::user()->name }}</a>
+            </div>
+        </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
-                <li >
-                    <a href="{{route('home')}}" class="nav-link {{ request()->is('/') ? 'active font-weight-bolder' : '' }}" >
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li>
+                    <a href="{{ route('home') }}"
+                        class="nav-link {{ request()->is('/') ? 'active font-weight-bolder' : '' }}">
                         <i class="far fa-circle nav-icon"></i>
-                            <p>Danh sách công việc</p>
+                        <p>Danh sách công việc</p>
                     </a>
                 </li>
                 <li>
-                    <a href={{route('work.index')}} class="nav-link {{ request()->routeIs('work*') ? 'active font-weight-bolder' : '' }}" >
+                    <a href={{ route('work.index') }}
+                        class="nav-link {{ request()->routeIs('work*') ? 'active font-weight-bolder' : '' }}">
                         <i class="far fa-circle nav-icon"></i>
-                            <p>Công việc cá nhân</p>
+                        <p>Công việc cá nhân</p>
                     </a>
                 </li>
             </ul>
@@ -76,4 +70,3 @@
 
     <!-- /.sidebar -->
 </aside>
-
