a1b3b7abf5a065cc74cc1a0ec2cd43708df41562
dvhungg123@gmail.com <dvhungg123@gmail.com>
2021-04-07 18:17:49 +0700

    comment api fuction

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 Route::get('/chart-api',[ManagerController::class,'chart'])->name('chart');
